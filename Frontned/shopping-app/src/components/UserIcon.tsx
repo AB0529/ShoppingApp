6827a98bba180028cf1fe5a4f6ad20473156fa21
AB0529 <supercrab115@gmail.com>
2022-05-21 14:47:48 -0400

    Frontend authentcation added

--- Frontned/shopping-app/src/components/UserIcon.tsx
+++ Frontned/shopping-app/src/components/UserIcon.tsx
@@ -1,9 +1,14 @@
 import { BiUserCircle } from "react-icons/bi";
+import { authenticationService } from "../auth/AuthService";
 
 function UserIcon() {
     return (
         <strong>
-            <BiUserCircle /> SussyBaka
+            {authenticationService.currentUserValue ? (
+                <>
+                    <BiUserCircle /> {authenticationService.currentUserValue}
+                </>
+            ) : (<a href="/login">Login</a>)}
         </strong>
     )
 }
