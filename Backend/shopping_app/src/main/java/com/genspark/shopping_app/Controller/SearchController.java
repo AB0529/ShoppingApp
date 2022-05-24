800be30538eaa19d63cc8739ef04fa6ee79be620
AB0529 <supercrab115@gmail.com>
2022-05-24 12:13:41 -0400

    Searchable items by tags implimented

---
+++ Backend/shopping_app/src/main/java/com/genspark/shopping_app/Controller/SearchController.java
@@ -0,0 +1,34 @@
+package com.genspark.shopping_app.Controller;
+
+import com.genspark.shopping_app.Entity.ApiResponse;
+import com.genspark.shopping_app.Entity.Item;
+import com.genspark.shopping_app.Service.Imp.ItemServiceImp;
+import com.genspark.shopping_app.Service.Imp.TagServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    @Autowired
+    private ItemServiceImp itemServiceImp;
+    @Autowired
+    private TagServiceImp tagServiceImp;
+
+    @GetMapping("/tag/{query}")
+    public ResponseEntity itemsFromTag(@PathVariable String query) {
+        Iterable<Object> items = tagServiceImp.getItemsByTagName(query);
+
+        if (!items.iterator().hasNext())
+            return new ResponseEntity(new ApiResponse("Item not found", null), HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity(new ApiResponse("Items found", items), HttpStatus.OK);
+    }
+}
--- Backend/shopping_app/src/main/java/com/genspark/shopping_app/Repository/TagRepository.java
+++ Backend/shopping_app/src/main/java/com/genspark/shopping_app/Repository/TagRepository.java
@@ -1,11 +1,19 @@
 package com.genspark.shopping_app.Repository;
 
+import com.genspark.shopping_app.Entity.Item;
 import com.genspark.shopping_app.Entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 //repo for tags
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Integer> {
+    @Query(value = "SELECT tbl_items.itemid FROM tbl_items WHERE tbl_items.itemid IN (SELECT tbl_items.itemid FROM tbl_items INNER JOIN tbl_tags ON tbl_tags.tag = :tag AND tbl_tags.item = tbl_items.itemid)", nativeQuery = true)
+    List<Integer> findItemsByTag(@Param("tag") String tag);
 }
