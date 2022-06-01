8a22f407ff6dbf3c0e76ac3e2cd15661602513e4
AB0529 <supercrab115@gmail.com>
2022-06-01 14:12:36 -0400

    Firebase

--- Backend/shopping_app/src/main/java/com/genspark/shopping_app/ShoppingAppApplication.java
+++ Backend/shopping_app/src/main/java/com/genspark/shopping_app/ShoppingAppApplication.java
@@ -5,6 +5,9 @@ import com.genspark.shopping_app.Service.Imp.PopulateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShoppingAppApplication {
@@ -13,4 +16,16 @@ public class ShoppingAppApplication {
 		SpringApplication.run(ShoppingAppApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
 }
