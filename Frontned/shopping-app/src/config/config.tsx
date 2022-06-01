92ad7c146b85cb69d8dd91503d2b22c92cf3856c
AB0529 <supercrab115@gmail.com>
2022-06-01 14:40:18 -0400

    Https

--- Frontned/shopping-app/src/config/config.tsx
+++ Frontned/shopping-app/src/config/config.tsx
@@ -1,6 +1,6 @@
 export default {
-	apiURL: "http://sr.esarnb.com:9080",
-	siteURL: "http://rainforest-deec7.web.app/",
+	apiURL: "https://sr.esarnb.com:9080",
+	siteURL: "https://rainforest-deec7.web.app/",
 	adminIDs: [58],
 	emailKey: 'service_diki0ch',
 	emailTemplate: 'template_04qokqf',
